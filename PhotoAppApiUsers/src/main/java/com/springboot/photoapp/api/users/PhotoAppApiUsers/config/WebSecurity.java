cb9e5400e766adcd5f88a0486a290ac7ceca5fcb
Sudipta <sudipta.anupam.datta@dohatec.com.bd>
2024-01-02 15:15:12 +0600

    Register authentication filter with HTTP Security

--- PhotoAppApiUsers/src/main/java/com/springboot/photoapp/api/users/PhotoAppApiUsers/config/WebSecurity.java
+++ PhotoAppApiUsers/src/main/java/com/springboot/photoapp/api/users/PhotoAppApiUsers/config/WebSecurity.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -26,6 +28,10 @@ public class WebSecurity {
     @Bean
     protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
+        AuthenticationManagerBuilder authenticationManagerBuilder =
+                http.getSharedObject(AuthenticationManagerBuilder.class);
+        AuthenticationManager authenticationManager = authenticationManagerBuilder.build();
+
         http.csrf().disable();
 
         http.authorizeHttpRequests()
@@ -33,6 +39,8 @@ public class WebSecurity {
                 .access(new WebExpressionAuthorizationManager("hasIpAddress('"+environment.getProperty("gateway.ip-address")+"')"))
                 .requestMatchers(new AntPathRequestMatcher("/h2-console/**")).permitAll()
                 .and()
+                .addFilter(new AuthenticationFilter(authenticationManager))
+                .authenticationManager(authenticationManager)
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
         http.headers().frameOptions().disable();
