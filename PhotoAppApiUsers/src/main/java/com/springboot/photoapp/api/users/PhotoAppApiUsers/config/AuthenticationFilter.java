1cfbe0ed34698a6a2ee371560837b225297ed831
Sudipta <sudipta.anupam.datta@dohatec.com.bd>
2024-01-03 11:55:38 +0600

    sucessfullAuthentication(): Generate JWT

--- PhotoAppApiUsers/src/main/java/com/springboot/photoapp/api/users/PhotoAppApiUsers/config/AuthenticationFilter.java
+++ PhotoAppApiUsers/src/main/java/com/springboot/photoapp/api/users/PhotoAppApiUsers/config/AuthenticationFilter.java
@@ -2,7 +2,10 @@ package com.springboot.photoapp.api.users.PhotoAppApiUsers.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.springboot.photoapp.api.users.PhotoAppApiUsers.dto.request.LoginRequestDto;
+import com.springboot.photoapp.api.users.PhotoAppApiUsers.dto.response.UserResponseDto;
 import com.springboot.photoapp.api.users.PhotoAppApiUsers.service.UserService;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -15,8 +18,14 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 import java.io.IOException;
+import java.time.Instant;
 import java.util.ArrayList;
+import java.util.Base64;
+import java.util.Date;
+import java.util.Objects;
 
 public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
@@ -58,5 +67,21 @@ public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 //        super.successfulAuthentication(request, response, chain, authResult);
 
         String userName = ((User) authResult.getPrincipal()).getUsername();
+        UserResponseDto userResponseDto = userService.findByUserName(userName);
+
+        String tokenSecret = environment.getProperty("token.secret");
+        byte[] secretKeyBytes = Base64.getEncoder().encode(tokenSecret.getBytes());
+        SecretKey secretKey = new SecretKeySpec(secretKeyBytes, SignatureAlgorithm.HS512.getJcaName());
+
+        String token = Jwts.builder()
+                .setSubject(userResponseDto.getUserId())
+                .setExpiration(Date.from(Instant.now().plusMillis(
+                        Long.parseLong(Objects.requireNonNull(environment.getProperty("token.expiration-time"))))))
+                .setIssuedAt(Date.from(Instant.now()))
+                .signWith(secretKey, SignatureAlgorithm.HS512)
+                .compact();
+
+        response.addHeader("token", token);
+        response.addHeader("userId", userResponseDto.getUserId());
     }
 }
